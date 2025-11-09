055d6c882b6793a93362dcba02989eba70e22c74
HemantRegmi <hregmi2060@gmail.com>
2025-11-09 11:42:10 +0545

    Initial commit: Adding Blood Bank Management System

--- sql/blood_bank_database.sql
+++ sql/blood_bank_database.sql
@@ -1,8 +1,7 @@
-create database blood_donation; /* creating database*/
 use blood_donation;
 
 /*create table donor_details in which all donor information gets stored.*/
-create table donor_details(
+create table if not exists donor_details(
 donor_id int auto_increment NOT NULL,
 donor_name varchar(50) NOT NULL,
 donor_number varchar(10) NOT NULL,
@@ -14,7 +13,7 @@ donor_address varchar(100) NOT NULL,
 Primary key(donor_id)
 );
 /*create table admin_info in which all admin information gets stored.*/
-create table admin_info(
+create table if not exists admin_info(
 admin_id int(10) NOT NULL UNIQUE AUTO_INCREMENT,
 admin_name varchar(50) NOT NULL,
 admin_username varchar(50) NOT NULL UNIQUE,
@@ -22,25 +21,25 @@ admin_password varchar(50) NOT NULL,
 Primary key(admin_id)
 );
 
-/*  insert admin data into admin_info table*/
-insert into admin_info(admin_name,admin_username,admin_password)
+/*  insert admin data into admin_info table (if not exists)*/
+insert ignore into admin_info(admin_name,admin_username,admin_password)
 values("hemant","hemantregmi004",123);
 
 /*create table blood in which all blood group is stored.*/
-create table blood(
+create table if not exists blood(
 blood_id int auto_increment Not Null,
 blood_group varchar(10) NOT NULL,
 primary key(blood_id)
 );
 
 /* insert all blood groups*/
-insert into blood(blood_group)
+insert ignore into blood(blood_group)
 values("B+"),("B-"),("A+"),("O+"),("O-"),("A-"),("AB+"),("AB-");
 
 
 
 /*create table contact_info in which your site contact information is stored.*/
-create table contact_info(
+create table if not exists contact_info(
 contact_id int auto_increment Not Null,
 contact_address varchar(100) NOT NULL,
 contact_mail varchar(50) NOT NULL,
@@ -48,12 +47,12 @@ contact_phone varchar(100) NOT NULL,
 primary key(contact_id)
 );
 
-insert into contact_info(contact_address,contact_mail,contact_phone)
+insert ignore into contact_info(contact_address,contact_mail,contact_phone)
 values("PatanDhoka, Lalitpur","bloodbank@gmail.com","9866717332");
     
     
 /*create table contact_query in which all querier inforamation gets stored.*/
-create table contact_query(
+create table if not exists contact_query(
 query_id int auto_increment Not Null,
 query_name varchar(100) NOT NULL,
 query_mail varchar(120) NOT NULL,
@@ -67,20 +66,20 @@ alter table contact_query modify column query_date
 timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
 
-insert into contact_query (query_name,query_mail,query_number,query_message) values
+insert ignore into contact_query (query_name,query_mail,query_number,query_message) values
 ("Hemant","hemant@gmail.com","9866717332","I need O+ Blood.");
 
 update contact_query set query_status="1" where query_id="1";
 
 /*create table query_stat in which query status is stored.*/
-CREATE TABLE query_stat(
+CREATE TABLE if not exists query_stat(
   id INT NOT NULL Unique,
  query_type VARCHAR(45) NOT NULL,
   PRIMARY KEY (id)
   );
   
   
-  insert into query_stat(id,query_type)
+  insert ignore into query_stat(id,query_type)
   values('1',"Read"),
   ('2',"Pending");
   
@@ -96,7 +95,7 @@ CREATE TABLE IF NOT EXISTS admin_notifications (
 );
 
 /*create table reservation in which all reservation information gets stored.*/
-CREATE TABLE reservation (
+CREATE TABLE if not exists reservation (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NULL,
     hospital_id INT NOT NULL,
@@ -114,7 +113,7 @@ CREATE TABLE reservation (
 );
 
 /*create table users in which all user information gets stored.*/
-CREATE TABLE users (
+CREATE TABLE if not exists users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -124,7 +123,8 @@ CREATE TABLE users (
 );
 
 /*create table hospitals in which all hospital information gets stored.*/
-CREATE TABLE hospitals (
+CREATE TABLE if not exists hospitals (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
