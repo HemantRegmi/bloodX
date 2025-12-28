1b4a3b7343fd76f660a5d565a5f53ee7c2057f9b
HemantRegmi <hregmi2060@gmail.com>
2025-12-28 19:45:06 +0545

    Fix: Add UpdateAutoScalingGroup permission to Jenkins role

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -62,7 +62,8 @@ resource "aws_iam_role_policy" "asg_policy" {
           "autoscaling:StartInstanceRefresh",
           "autoscaling:CancelInstanceRefresh",
           "autoscaling:DescribeInstanceRefreshes",
-          "autoscaling:CompleteLifecycleAction"
+          "autoscaling:CompleteLifecycleAction",
+          "autoscaling:UpdateAutoScalingGroup"
         ]
         Resource = "*"
       }
