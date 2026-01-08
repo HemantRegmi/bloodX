f8863d31a566ba5e8268c4d87324acfdccf41a14
HemantRegmi <hregmi2060@gmail.com>
2026-01-08 10:51:09 +0545

    demo

--- terraform/run_fix_v3.ps1
+++ terraform/run_fix_v3.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $ScriptDir
 
-$BastionIP = "65.1.48.32"
+$BastionIP = "52.66.176.18"
 $PrivateIP = "10.0.11.20"
 
 Write-Host "Auto-Fixer V3 (Line-Ending Fix)"
